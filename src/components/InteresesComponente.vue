cefdf6078468296061138d39b73675c02281707c
andydmz <anddom179@gmail.com>
2024-11-08 19:26:57 -0300

    Portafolio 2

---
+++ src/components/InteresesComponente.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref } from 'vue';
+// Este es un arreglo con ref para que se pueda reactivar el cambio de los intereses
+const intereses = ref([
+    'Mi interés principal, además de la programación, es la música. Actualmente llevo 4 años tocando el piano y tocando en restaurantes y en prestigiosas bodegas. La música es lo que más disfruto en la vida.',
+    'Tengo un interés particular en los videojuegos desde pequeño, y siempre me interesó algún día poder crear un videojuego con mis compañeros de tecnicatura.',
+    'Me encanta el desarrollo frontend porque me permite combinar creatividad y tecnología para crear interfaces atractivas y funcionales. Disfruto diseñar experiencias de usuario intuitivas y visualmente agradables.',
+    'Me gusta mucho la programación porque su mundo abarca muchas cosas nuevas todos los días, y me gusta el hecho de crear algo con la programación.'
+]);
+</script>
+
+<template>
+    <div class="intereses-contenedor">
+        <FondoLava />
+        <ul class="contenedor-lista">
+            <li class="item" v-for="interes in intereses" :key="interes">
+                {{ interes }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.intereses-contenedor {
+    display: flex;
+    flex-direction: column; /* Para que los elementos se apilen verticalmente */
+    align-items: center; /* Centra la lista horizontalmente */
+    position: relative;
+    margin: 0 auto;
+    max-width: 85%;
+    padding: 2rem; /* Espaciado interior más amplio */
+    background: hsl(29, 100%, 50%); /* Fondo transparente con color blanco */
+    border-radius: 12px; /* Bordes redondeados */
+    box-shadow: 0 4px 15px rgba(0, 0, 0, 0.1); /* Sombra suave */
+}
+
+.contenedor-lista {
+    list-style-type: none;
+    padding: 0; /* Elimina el padding de la lista */
+    margin: 0; /* Elimina el margen de la lista */
+    color: #D9E0E8; /* Color de texto oscuro */
+    font-size: 1.2rem; /* Tamaño de fuente más pequeño y elegante */
+}
+
+.item {
+    margin-bottom: 1rem;
+    padding: 1rem; /* Espaciado alrededor de cada ítem */
+    background: rgba(106, 199, 227, 0.2); /* Fondo celeste semi-transparente */
+    border: 1px solid #e1e1e1; /* Borde sutil alrededor de cada ítem */
+    border-radius: 8px; /* Bordes redondeados para cada ítem */
+    transition: none; /* Sin efecto de hover */
+}
+</style>
---
+++ src/components/ProyectosComponente.vue
@@ -0,0 +1,158 @@
+<script setup>
+import proyecto1 from '/src/assets/portafolioCara.png';
+import proyecto2 from '/src/assets/PortafolioSobreMi.png';
+import proyecto3 from '/src/assets/Skills.png';
+import proyecto4 from '/src/assets/Experiencia.png';
+
+import { ref } from 'vue';
+var misProyectos = ref([
+    {
+        id: 1,
+        src: proyecto1,
+        titulo: "Primer Portafolio",
+        descripcion: "Aqui cree mi primer portafolio personal, este portafolio se basa mas en una presentacion mia.",
+        projectoLink: "https://www.google.com.ar",
+
+    },
+    {
+        id: 2,
+        src: proyecto2,
+        titulo: "Sobre Mi.",
+        descripcion: "Esta seccion habla un poco sobre mis datos e intereses personales.",
+        projectoLink: "https://www.google.com.ar",
+        githubLink: "https://github.com/usuario/proyecto"
+    },
+    {
+        id: 3,
+        src: proyecto3,
+        titulo: "Skills/Habilidades.",
+        descripcion: "Aqui una seccion que habla sobre las habilidades que tengo y he obtenido estudiando sobre la programacion.",
+        projectoLink: "https://www.google.com.ar",
+        githubLink: "https://github.com/usuario/proyecto"
+    },
+    {
+        id: 4,
+        src: proyecto4,
+        titulo: "Educacion y Experiencia laboral",
+        descripcion: "Esta seccion habla sobre mi experiencia en trabajos.",
+        projectoLink: "https://www.google.com.ar",
+        githubLink: "https://github.com/usuario/proyecto"
+    },
+]);
+</script>
+
+<template>
+    <div class="galeria">
+        <ul>
+            <li class="proyecto" v-for="proyecto in misProyectos" :key="proyecto.id">
+                <img :src="proyecto.src" :alt="proyecto.titulo">
+                <div class="proyecto-info">
+                    <h3>{{ proyecto.titulo }}</h3>
+                    <p>{{ proyecto.descripcion }}</p>
+
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.galeria {
+    width: 100%;
+    padding: 15px; /* Aumentar el padding para un fondo más ancho */
+    background: linear-gradient(-45deg, #021526, #ff8c00, #ff8400, #E2E2B6);
+    background-size: 400% 400%;
+    animation: gradient 15s ease infinite;
+}
+
+@keyframes gradient {
+    0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
+}
+
+ul {
+    list-style-type: none; /* Eliminar viñetas de la lista */
+    padding: 0; /* Eliminar padding de la lista */
+    display: flex; /* Usar flexbox para disposición horizontal */
+    flex-wrap: wrap; /* Permitir que los elementos se envuelvan */
+    justify-content: center; /* Centrar elementos horizontalmente */
+}
+
+.proyecto {
+    display: flex;
+    flex-direction: column;
+    border: 2px solid #ddd;
+    border-radius: 12px;
+    overflow: hidden;
+    background-color: rgba(255, 255, 255, 0.85); /* Fondo más tranquilo y claro para las tarjetas */
+    max-width: 380px; /* Aumentar el ancho máximo de las tarjetas */
+    margin: 15px; /* Margen entre proyectos */
+    box-shadow: 0 4px 15px rgba(0, 0, 0, 0.1); /* Sombra sutil */
+    transition: transform 0.3s, box-shadow 0.3s; /* Transición suave */
+}
+
+.proyecto:hover {
+    transform: translateY(-5px); /* Elevar al pasar el mouse */
+    box-shadow: 0 8px 30px rgba(0, 0, 0, 0.2); /* Aumentar sombra al pasar el mouse */
+}
+
+.proyecto img {
+    width: 100%;
+    height: auto;
+    display: block;
+}
+
+.proyecto-info {
+    padding: 15px;
+    text-align: center;
+}
+
+.proyecto-info h3 {
+    margin: 10px 0;
+    font-size: 1.5em; /* Aumentar tamaño del título */
+    color: #67C6E3;
+}
+
+.proyecto-info p {
+    margin: 10px 0;
+    font-size: 1em;
+    color: #3c3c3c;
+}
+
+.proyecto-links {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin-top: 20px; /* Ajustar margen superior */
+}
+
+.proyecto-links .btn-ver-mas {
+    background-color: #c889c7;
+    color: #fff;
+    padding: 10px 15px;
+    border-radius: 5px;
+    text-decoration: none;
+    transition: background-color 0.3s;
+}
+
+.proyecto-links .btn-ver-mas:hover {
+    background-color: #9b739a; /* Azul más oscuro */
+}
+
+.proyecto-links .github-link {
+    color: #3c3c3c;
+    text-decoration: none;
+    font-size: 0.9em;
+}
+
+.proyecto-links .github-link:hover {
+    text-decoration: underline; /* Subrayar al pasar el mouse */
+}
+</style>
